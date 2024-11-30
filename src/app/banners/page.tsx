527de1d62067b5e6107ec93eff3cfc55b6748d96
Ahmed <ahmedmemon3344@gmail.com>
2024-11-30 19:13:53 +0500

    ecom2

--- src/app/banners/page.tsx
+++ src/app/banners/page.tsx
@@ -1,15 +1,32 @@
 import Image from "next/image"
+import { FaDesktop } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { GiClothes } from "react-icons/gi";
+import { MdMedicalServices } from "react-icons/md";
+import { IoManSharp } from "react-icons/io5";
+import { IoMdContact } from "react-icons/io";
 export default function Banner() {
     return (
         <div className="Parentbanner">
-            <ul className="Childbanner">
-                <li className="home">Home</li>
-                <li className="sales">Sales</li>
-                <li className="products">All Products</li>
-                <li className="services">Our Services</li>
-                <li className="about">About Us</li>
-                <li className="contact">Contact Us</li>
+                <label>
+                    <input type="checkbox" className="checkbox" />
+                    <div className="toggle">
+                        <span className="top_line common"></span>
+                        <span className="middle_line common"></span>
+                        <span className="bottom_line common"></span>
+                    </div>
+                    <div className="slide">
+<h1>MENU</h1>
+<ul className="Childbanner">
+                <li ><a href="#"><FaDesktop className="icon"/>Home</a></li>
+                <li ><a href="#"><FaUser className="icon"/>Sales</a></li>
+                <li ><a href="#"><GiClothes className="icon"/>Products</a></li>
+                <li ><a href="#"><MdMedicalServices className="icon"/>Services</a></li>
+                <li ><a href="#"><IoManSharp className="icon"/>About Us</a></li>
+                <li ><a href="#"><IoMdContact className="icon"/>Contact Us</a></li>
             </ul>
+            </div>
+            </label>
             <div className="posters">
                 <Image src={"/banner1.jpg"} alt="Banner" width={600} height={500} className="bigbanner" />
             </div>
--- src/app/headingsale/page.tsx
+++ src/app/headingsale/page.tsx
@@ -1,11 +1,8 @@
-import { Steps } from "antd"
+import Image from "next/image"
 export default function Heading(){
     return(
         <div className="Parenthead">
             <div className="Childhead">
-            <Steps/>
-             <h1>Flash Sale</h1>
-             <h3>December Winter Flash Sale For You</h3>
                 <button>Shop All Products</button>
                 <p>50%-20% Sale</p>
             </div>
