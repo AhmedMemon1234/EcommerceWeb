0910cd95d5a384bc8f50b7fc2f13532352f1c004
Ahmed <ahmedmemon3344@gmail.com>
2024-12-04 01:32:28 +0500

    ecom2

--- src/app/watch cart/page.tsx
+++ src/app/watch cart/page.tsx
@@ -136,14 +136,14 @@ export default function Watchcart() {
                     <div className="mt-8">
                         <div>
                             <h3 className="text-xl font-bold text-gray-800">Product Description</h3>
-                            <p className="text-sm text-gray-800 mt-4">Elevate your casual style with our premium men's t-shirt. Crafted for comfort and designed with a modern fit, this versatile shirt is an essential addition to your wardrobe. The soft and breathable fabric ensures all-day comfort, making it perfect for everyday wear. Its classNameic crew neck and short sleeves offer a timeless look.</p>
+                            <p className="text-sm text-gray-800 mt-4">Elevate your casual style with our premium mens t-shirt. Crafted for comfort and designed with a modern fit this versatile shirt is an essential addition to your wardrobe. The soft and breathable fabric ensures all-day comfort making it perfect for everyday wear. Its classNameic crew neck and short sleeves offer a timeless look.</p>
                         </div>
 
                         <ul className="space-y-3 list-disc mt-4 pl-4 text-sm text-gray-800">
                             <li>A t-shirt is a wardrobe essential because it is so versatile.</li>
                             <li>Available in a wide range of sizes, from extra small to extra large, and even in tall and petite sizes.</li>
                             <li>This is easy to care for. They can usually be machine-washed and dried on low heat.</li>
-                            <li>You can add your own designs, paintings, or embroidery to make it your own.</li>
+                            <li>You can add your own designs paintings, or embroidery to make it your own.</li>
                         </ul>
                     </div>
                 </div>
