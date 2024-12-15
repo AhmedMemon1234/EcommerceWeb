bd59f3cd24f2653e35f4a20802cefef7a1f2c054
Ahmed <ahmedmemon3344@gmail.com>
2024-12-15 23:01:31 +0500

    Ecommerceweb

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['readymadeui.com'], // Allowed image domains
   },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors during builds
+  },
 };
 
 export default nextConfig;
